5becaa657dbccd539c1356f2fe8c596fff741b8c
Michael Gale <gale.michael@gmail.com>
2017-09-24 17:30:10 -0600

    Adding custom install list for min install

---
+++ mgale-min-install.sh
@@ -0,0 +1,9 @@
+cd /opt/openstack-ansible/playbooks
+openstack-ansible setup-hosts.yml
+openstack-ansible setup-infrastructure.yml
+openstack-ansible os-keystone-install.yml
+openstack-ansible os-glance-install.yml
+openstack-ansible os-nova-install.yml
+openstack-ansible os-neutron-install.yml
+openstack-ansible os-horizon-install.yml
+
